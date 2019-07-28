90d101d89fec497bfd387622bc3781b2dd0f5f60
Togekey <zzyzzy@outlook.com>
2019-07-28 23:52:59 +0800

    geometry

---
+++ camera_ray.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <cstdlib>
+#include <memory>
+#include <vector>
+#include <utility>
+#include <cstdint>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <limits>
+
+#include "geometry.h"
